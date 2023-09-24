e96c49e144a1506d26b4f418fa68570597db0885
unknown <reptor352@gmail.com>
2023-09-24 18:50:12 +0530

    added

--- website/website/websiteapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from. models import table
-from. models import team
-admin.site.register(table)
-admin.site.register(team)
--- website/website/websiteapp/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-  path('',views.home, name='home'),
-  path('index.html/',views.index, name='index'),
-  path('about.html/', views.about, name='about')
-
-    ]
