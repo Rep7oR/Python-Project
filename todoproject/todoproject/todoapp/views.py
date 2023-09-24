2a38e6ac226b3372b2a87f8edcac09bce84e56c3
unknown <reptor352@gmail.com>
2023-09-24 10:57:52 +0530

    added

---
+++ todoproject/todoproject/todoapp/views.py
@@ -0,0 +1,62 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+from .models import Task
+from .forms import TodoForm
+from django.views.generic import ListView
+from django.views.generic import DeleteView
+from django.views.generic.edit import UpdateView, DeleteView
+
+# Create your views here.
+class tasklistview(ListView):
+    model = Task
+    template_name = 'index.html'
+    context_object_name = 'task1'
+
+
+class taskdetailview(DeleteView):
+    model = Task
+    template_name = 'details.html'
+    context_object_name = 'task'
+
+class taskupdate(UpdateView):
+    model = Task
+    template_name = 'update.html'
+    context_object_name = 'task'
+    fields = ('name', 'priority', 'date')
+    def get_success_url(self):
+        return reverse_lazy('detail', kwargs={'pk':self.object.id})
+class taskdelete(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('home')
+
+def home(request):
+    task1 = Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('task', ' ')
+        priority = request.POST.get('priority', '')
+        date = request.POST.get('date', '')
+        task = Task(name=name, priority=priority, date=date)
+        task.save()
+    return render(request, 'index.html', {'task1': task1})
+#
+#
+# def delete(request, taskid):
+#     task = Task.objects.get(id=taskid)
+#     if request.method == "POST":
+#         task.delete()
+#         return redirect('/')
+#
+#     return render(request, 'delete.html')
+#
+#
+# def update(request, id):
+#     task = Task.objects.get(id=id)
+#     form = TodoForm(request.POST or None, instance=task)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('/')
+#     return render(request, 'edit.html', {'form': form, 'task': task})
